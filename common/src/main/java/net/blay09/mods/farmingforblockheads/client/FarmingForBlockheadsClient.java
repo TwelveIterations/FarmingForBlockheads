67d1da916d3495e80ccc55c81b09da8d0523f3ca
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-27 23:23:20 +0100

    feat: Update to Minecraft 1.21.11

--- common/src/main/java/net/blay09/mods/farmingforblockheads/client/FarmingForBlockheadsClient.java
+++ common/src/main/java/net/blay09/mods/farmingforblockheads/client/FarmingForBlockheadsClient.java
@@ -1,6 +1,18 @@
 package net.blay09.mods.farmingforblockheads.client;
 
+import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.client.BalmClientRegistrars;
+import net.blay09.mods.balm.platform.compatibility.recipeviewer.RecipeViewerInfoProvider;
+import net.blay09.mods.balm.platform.compatibility.recipeviewer.RecipeViewerRegistrar;
+import net.blay09.mods.farmingforblockheads.block.ModBlocks;
+import net.blay09.mods.farmingforblockheads.client.gui.screen.MarketScreen;
+import net.blay09.mods.farmingforblockheads.recipe.MarketRecipe;
+import net.blay09.mods.farmingforblockheads.recipe.ModRecipes;
+import net.blay09.mods.farmingforblockheads.registry.MarketDefaultsRegistry;
+import net.minecraft.client.renderer.Rect2i;
+import net.minecraft.network.chat.Component;
+
+import static net.blay09.mods.farmingforblockheads.FarmingForBlockheads.id;
 
 public class FarmingForBlockheadsClient {
 
@@ -8,6 +20,29 @@ public class FarmingForBlockheadsClient {
         registrars.menuScreens(ModScreens::initialize);
         registrars.entityRenderers(ModRenderers::initialize);
         registrars.blockEntityRenderers(ModRenderers::initialize);
-    }
 
+
+        Balm.modSupport().recipeViewers().register(id("recipes"), new RecipeViewerInfoProvider() {
+            @Override
+            public void initialize(RecipeViewerRegistrar registrar) {
+                registrar.registerRecipeType(id("market"), MarketRecipe.class)
+                        .withSyncedRecipes(ModRecipes.marketRecipe)
+                        .withCraftingStation(ModBlocks.market)
+                        .buildDisplay(display -> display
+                                .title(Component.translatable("jei.farmingforblockheads.market"))
+                                .icon(ModBlocks.market)
+                                .size(86, 48)
+                                .background(id("textures/gui/jei_market.png"))
+                                .slots((recipe, slots) -> {
+                                    final var payment = MarketDefaultsRegistry.resolvePayment(recipe);
+                                    slots.inputSlot(16, 13).add(payment.ingredient());
+                                    slots.outputSlot(54, 13).add(recipe.result());
+                                }));
+
+                registrar.registerScreenOcclusion(MarketScreen.class, marketScreen -> marketScreen.getFilterButtons().stream()
+                        .map(button -> new Rect2i(button.getX(), button.getY(), button.getWidth(), button.getHeight()))
+                        .toList());
+            }
+        });
+    }
 }
