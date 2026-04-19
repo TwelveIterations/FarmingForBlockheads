3ff7af1a37f725d0ae6b5622cdd072cf4570173a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-04-19 13:33:00 +0200

    feat: Add dedicated category for spawn eggs, if enabled

--- common/src/main/java/net/blay09/mods/farmingforblockheads/api/MarketCategories.java
+++ common/src/main/java/net/blay09/mods/farmingforblockheads/api/MarketCategories.java
@@ -6,5 +6,6 @@ public class MarketCategories {
     public static final Identifier SEEDS = Identifier.fromNamespaceAndPath("farmingforblockheads", "seeds");
     public static final Identifier SAPLINGS = Identifier.fromNamespaceAndPath("farmingforblockheads", "saplings");
     public static final Identifier FLOWERS = Identifier.fromNamespaceAndPath("farmingforblockheads", "flowers");
+    public static final Identifier SPAWN_EGGS = Identifier.fromNamespaceAndPath("farmingforblockheads", "spawn_eggs");
     public static final Identifier OTHER = Identifier.fromNamespaceAndPath("farmingforblockheads", "other");
 }
